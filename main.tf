1af94b460b549279634915e92177cfe1dd21ce36
Alexander Mancevice <amancevice@cargometrics.com>
2018-06-12 07:07:14 -0400

    add md5 to cloud filename

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ provider "template" {
 }
 
 locals {
-  version = "0.3.0"
+  version = "0.4.0"
 
   dialog {
     callback_id  = "${var.callback_id}"
@@ -99,7 +99,7 @@ resource "google_cloudfunctions_function" "function" {
 
 module "slash_command" {
   source                          = "amancevice/slack-slash-command/google"
-  version                         = "0.3.2"
+  version                         = "0.4.0"
   auth_channels_exclude           = ["${var.slash_command_auth_channels_exclude}"]
   auth_channels_include           = ["${var.slash_command_auth_channels_include}"]
   auth_channels_permission_denied = "${var.slash_command_auth_channels_permission_denied}"
